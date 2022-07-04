1e1ce9e62f7ed6d291907ea26e67ddc4fc5a257a
River <+>
2022-07-03 23:34:42 -0400

    commit tree 3b63553b8f225111652b09f15db9a2eed2015767

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :click_bait 
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:%w(Fiction Non-Fiction)}
+
+    def click_bait
+        unless title.present? &&
+            title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, 'not allowed')
+        end
+    end
 end
